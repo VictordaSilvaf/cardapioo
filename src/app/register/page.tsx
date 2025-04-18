962f27bd5a519bf5cd84ad0e05dd3361537017e6
VictordaSilvaf <victordasilvafernandes@gmail.com>
2025-04-18 18:42:01 -0300

    feat: add registration and private page functionality
    - Implemented a new private page that redirects unauthenticated users to the login page.
    - Created a registration page with form fields for name, email, password, and confirmation.
    - Added a dialog component for UI consistency across the application.
    - Introduced Supabase client utilities for both server and browser environments.
    - Implemented middleware for session management with Supabase.
    - Configured TypeScript settings for improved development experience.

---
+++ src/app/register/page.tsx
@@ -0,0 +1,111 @@
+import Image from "next/image";
+import { login, signup } from "../login/actions";
+import Link from "next/link";
+
+export default function RegisterPage() {
+    return (
+        <>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                    <Image
+                        alt="Sua Empresa"
+                        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                        className="mx-auto h-10 w-auto"
+                        width={40}
+                        height={40}
+                    />
+                    <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                        Crie sua conta agora mesmo
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+                    <div className="bg-white px-6 py-12 shadow-sm sm:rounded-lg sm:px-12">
+                        <form action="#" method="POST" className="space-y-6">
+                            <div>
+                                <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">
+                                    Nome completo
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="name"
+                                        name="name"
+                                        type="text"
+                                        required
+                                        autoComplete="name"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                                    Endereço de e-mail
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        required
+                                        autoComplete="email"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                                    Senha
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        required
+                                        autoComplete="new-password"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <label htmlFor="confirm-password" className="block text-sm/6 font-medium text-gray-900">
+                                    Confirme sua senha
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="confirm-password"
+                                        name="confirm-password"
+                                        type="password"
+                                        required
+                                        autoComplete="new-password"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <button
+                                    formAction={signup}
+                                    type="submit"
+                                    className="cursor-pointer flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Criar conta
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <p className="mt-10 text-center text-sm/6 text-gray-500">
+                        Já possui uma conta?{' '}
+                        <Link href={'/login'} className="cursor-pointer font-semibold text-indigo-600 hover:text-indigo-500">
+                            Entrar
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
